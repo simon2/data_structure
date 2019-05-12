c772c159f863ca0c382975ed3a1995da486a7421
simon2 <bzy19921017@gmail.com>
2019-05-12 10:20:00 +0900

    fixed bugs in linkedList.c

--- linkedList.c
+++ linkedList.c
@@ -8,39 +8,39 @@ struct Node{
   Node* next;
 };
 
-Node createNode(int value);
-void insert(Node* node, int x, int index);
+Node* createNode(int value);
+Node* insert(Node* node, int x, int index);
 void delete(Node* node, int index);
-void travel(Node node);
-int get(Node node, int index);
+void travel(Node* node);
+int get(Node* node, int index);
 
 int main(){
   int firstV;
   printf("Please input the value for the first node:");
-  scanf("%ld ",&firstV);
-  Node node = createNode(firstV);
-  insert(&node,5,1);
+  scanf("%ld",&firstV);
+  Node* node = createNode(firstV);
+  insert(node,5,1);
   travel(node);
-  insert(&node,10,2);
+  insert(node,10,2);
   travel(node);
-  insert(&node,15,3);
+  insert(node,15,3);
   travel(node);
-  delete(&node,3);
+  delete(node,3);
   travel(node);
-  delete(&node,1);
+  delete(node,1);
   travel(node);
   get(node,2);
   return 0;
 }
 
-Node createNode(int value){
-  Node node;
-  node.value = value;
-  node.next = NULL;
+Node* createNode(int value){
+  Node* node = (Node*)malloc(sizeof(Node));
+  node->value = value;
+  node->next = NULL;
   return node;
 }
 
-void insert(Node* node, int x, int index){
+Node* insert(Node* node, int x, int index){
   int i;
   Node* temp = node;
   for(i=0;i<index-1;i++){
@@ -50,9 +50,10 @@ void insert(Node* node, int x, int index){
       temp = temp -> next;
     }
   }
-  Node newNode = createNode(x);
-  newNode.next = temp->next;
-  temp->next = &newNode;
+  Node* newNode = createNode(x);
+  newNode->next = temp->next;
+  temp->next = newNode;
+  return node;
 }
 
 void delete(Node* node, int index){
@@ -74,18 +75,18 @@ void delete(Node* node, int index){
   }
 }
 
-int get(Node node, int index){
+int get(Node* node, int index){
   int i;
-  Node* temp = &node;
+  Node* temp = node;
   for(i=0;i<index;i++){
     temp = temp -> next;
   }
   return temp->value;
 }
 
-void travel(Node node){
+void travel(Node* node){
   int i;
-  Node* temp = &node;
+  Node* temp = node;
   while(temp != NULL){
     printf("%ld ",temp->value);
     temp = temp -> next;
